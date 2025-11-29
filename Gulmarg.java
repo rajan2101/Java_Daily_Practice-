f4e334cd30b394396a5480425583f90f13896d83
rajan2101 <rajanpersonal01@gmail.com>
2025-11-29 21:33:28 +0530

    Add files via upload

---
+++ Gulmarg.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+class Gulmarg extends HillStations {
+    public void location() {
+        System.out.println("Gulmarg is located in Jammu and Kashmir");
+    }
+
+    public void famousFor() {
+        System.out.println("Gulmarg is famous for skiing and the Gulmarg Gondola");
+    }
+}
+

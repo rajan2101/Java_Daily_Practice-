e80c2cca331c3e023144e6ff52655e45eeda08dc
rajan2101 <rajanpersonal01@gmail.com>
2025-11-30 21:00:10 +0530

    Add files via upload

---
+++ HillStations.java
@@ -0,0 +1,11 @@
+package Lab3;
+
+class HillStations {
+    public void location() {
+        System.out.println("Generic hill station location");
+    }
+
+    public void famousFor() {
+        System.out.println("Generic hill station specialty");
+    }
+}

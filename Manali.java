cf323fd47315fc187906d630a093bedf8f7a2a62
rajan2101 <rajanpersonal01@gmail.com>
2025-12-02 16:52:55 +0530

    Add files via upload

---
+++ Manali.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+class Manali extends HillStations {
+    public void location() {
+        System.out.println("Manali is located in Himachal Pradesh");
+    }
+
+    public void famousFor() {
+        System.out.println("Manali is famous for snow sports and scenic beauty");
+    }
+}
+

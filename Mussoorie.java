497c8721368a27f7596e2845f950d82e763e0d4e
rajan2101 <rajanpersonal01@gmail.com>
2025-12-03 21:54:54 +0530

    Add files via upload

---
+++ Mussoorie.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+class Mussoorie extends HillStations {
+    public void location() {
+        System.out.println("Mussoorie is located in Uttarakhand");
+    }
+
+    public void famousFor() {
+        System.out.println("Mussoorie is famous for colonial charm and waterfalls");
+    }
+}
+

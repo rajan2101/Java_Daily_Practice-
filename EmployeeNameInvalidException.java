4b1e153ab28356379e9f95385db24939a60260ac
rajan2101 <rajanpersonal01@gmail.com>
2025-12-25 17:14:02 +0530

    Add files via upload

---
+++ EmployeeNameInvalidException.java
@@ -0,0 +1,13 @@
+package Lab4;
+
+public class EmployeeNameInvalidException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeNameInvalidException(String message) {
+        super(message);
+    }
+}
